32ad0717c68ee701e6112eae5d5c14d2009d2321
Anand Rajendran <rajendran.anand@live.com>
2024-08-16 12:25:30 +0530

    aws script updated

--- aws_script.sh
+++ aws_script.sh
@@ -1,5 +1,6 @@
 docker rm $(docker ps -a -f status=exited -q)
 docker rmi $(docker images -a -q)
 docker builder prune -a
+git pull https://github.com/zollyzeus/ASR-Deployment.git
 docker build -t asrproject .
 docker run -p 5000:5000 asrproject
